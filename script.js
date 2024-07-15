5850e1eb76d9a648db7dc0c8d77ed5c116cbb2fc
Isaac Bailey <isaacroybailey@icloud.com>
2024-07-15 10:56:00 +0100

    Finished Cycling and Resetting Game

--- script.js
+++ script.js
@@ -13,15 +13,25 @@ console.log("Rock, Paper or Scissors")
 var playerscore = 0
 var computerscore = 0
 
+var slideimages = new Array();
+slideimages[0] = new Image();
+slideimages[0].src = "images/rock.jpeg";
+slideimages[1] = new Image();
+slideimages[1].src = "images/paper.jpeg";
+slideimages[2] = new Image();
+slideimages[2].src = "images/scissors.png";
+
+var step = 0
+var play = true
+
 function playRound(e){
     const choice = e.target.id;
-    const opponent = choose();
+    let opponent = choose();
     if(!choice){
         return
     }
     console.log(opponent)
     const result = document.getElementById("result");
-    result.textContent=opponent;
     if (choice == opponent){
         result.textContent = "Draw";
     }
@@ -31,6 +41,37 @@ function playRound(e){
     else{
         result.textContent = "I win";
     }
+    play = false;
+    document.getElementById("slide").src = slideimages[values[opponent]-1].src
+    document.getElementById("rock").disabled = true;
+    document.getElementById("paper").disabled = true;
+    document.getElementById("scissors").disabled = true;
+    document.getElementById("reset").style.visibility = 'visible';
+    if (choice == "reset"){
+        play = true;
+        document.getElementById("rock").disabled = false;
+        document.getElementById("paper").disabled = false;
+        document.getElementById("scissors").disabled = false;
+        document.getElementById("reset").style.visibility = 'hidden';
+        document.getElementById("result").textContent = "VS"
+    }
 }
 
-document.addEventListener("click",(e) => playRound(e))
+function slideit(){
+    if (play){
+        let image = document.getElementById("slide");
+        image.src = slideimages[step].src;
+        if (step<2){
+            step++;
+        }
+        else{
+            step = 0;
+        }
+    }
+    setTimeout("slideit()",500);
+}
+
+
+document.addEventListener("DOMContentLoaded", () => slideit());
+
+document.addEventListener("click",(e) => playRound(e));
